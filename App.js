290dbc9e9c497653534148fcd5d893127141f644
daniloop404 <daniloop404@gmail.com>
2023-12-12 11:21:21 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState(0);
+
+  const handleAddition = () => {
+    setResult(Number(num1) + Number(num2));
+  };
+
+  const handleSubtraction = () => {
+    setResult(Number(num1) - Number(num2));
+  };
+
+  const handleMultiplication = () => {
+    setResult(Number(num1) * Number(num2));
+  };
+
+  const handleDivision = () => {
+    if (Number(num2) !== 0) {
+      setResult(Number(num1) / Number(num2));
+    } else {
+      alert('No se puede dividir por cero');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calculadora</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Número 1"
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={(text) => setNum1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Número 2"
+        keyboardType="numeric"
+        value={num2}
+        onChangeText={(text) => setNum2(text)}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Sumar" onPress={handleAddition} />
+        <Button title="Restar" onPress={handleSubtraction} />
+        <Button title="Multiplicar" onPress={handleMultiplication} />
+        <Button title="Dividir" onPress={handleDivision} />
+      </View>
+      <Text>Resultado: {result}</Text>
     </View>
   );
 }
@@ -16,5 +60,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginVertical: 10,
+    padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
   },
 });
